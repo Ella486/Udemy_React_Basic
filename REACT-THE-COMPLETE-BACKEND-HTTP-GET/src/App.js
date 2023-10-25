20d431e36158adb7ca80f060829745c642008103
mjlee <mjlee@4net.co.kr>
2023-10-25 10:52:35 +0900

    react backend first commit

---
+++ REACT-THE-COMPLETE-BACKEND-HTTP-GET/src/App.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // 첫번째 방법
+  // function fetchMoviesHandler() {
+  //   fetch('https://swapi.dev/api/films/')
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseDate: movieData.release_date
+  //         };
+  //       });
+  //       setMovies(transformedMovies);
+  //     });
+  // }
+
+  // 두번째 방법 비동기화 코드(추후 update된 code)
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const reponse = await fetch('https://swapi.dev/api/films/');
+      if (!reponse.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await reponse.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+
+  let content = <p>Found no movies.</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
