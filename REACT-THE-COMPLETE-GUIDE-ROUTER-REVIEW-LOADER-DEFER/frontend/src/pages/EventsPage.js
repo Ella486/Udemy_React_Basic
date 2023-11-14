dd46299d8f8dca31c8366d031b82f030e73f5c22
mjlee <mjlee@4net.co.kr>
2023-11-14 15:45:10 +0900

    react router loader defer

---
+++ REACT-THE-COMPLETE-GUIDE-ROUTER-REVIEW-LOADER-DEFER/frontend/src/pages/EventsPage.js
@@ -0,0 +1,49 @@
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
+
+import EventsList from '../components/EventsList';
+
+/**
+ * Suspense
+ * - 다른 데이터가 도착하길 기다리는 동안에 폴백(fallback)을 보여주는 
+ * 특정한 상황에서 사용할 수 있다.
+ */
+function EventsPage() {
+    const {events} = useLoaderData();
+
+    return (
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    );
+}
+
+export default EventsPage;
+
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+
+    if (!response.ok) {
+        //return { isError: true, message: 'Could not fetch events.' };
+
+        // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+        //     status: 500
+        // });
+
+        throw json(
+            { message: 'Could not fetch events.' },
+            { status: 500 }
+        );
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents() // Promise
+    });
+}
